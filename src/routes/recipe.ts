744e2294112c53ce245534d841cc71663d4c2454
ellunen <ellu.nenonen@gmail.com>
2022-05-22 16:01:25 +0300

    add /user to route

--- src/routes/recipe.ts
+++ src/routes/recipe.ts
@@ -27,7 +27,7 @@ router.get('/all', async (req, res, next) => {
   }
 });
 
-router.get('/:userid', async (req, res, next) => {
+router.get('/user/:userid', async (req, res, next) => {
   try {
     const allRecipes = await Recipe.find({ userid: req.params.userid });
     res.status(200).json(allRecipes);
